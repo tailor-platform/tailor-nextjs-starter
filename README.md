2465d38f7902feff69c26e05aa7e5eb7f667d229
IzumiSy <beetle-noise@gmx.com>
2024-01-15 16:54:44 +0900

    Initial design

---
+++ README.md
@@ -0,0 +1,53 @@
+# tailor-nextjs-starter
+
+This is a base [Next.js](https://nextjs.org/) application to get you started developing a frontend for your Tailor application.
+
+## Features
+
+- NodeJS 18.
+- [pnpm](https://pnpm.io) for package management.
+- [graphql-codegen](https://the-guild.dev/graphql/codegen) to auto-generate code based on your Tailor application's GraphQL schema and operations.
+
+## Getting Started
+
+### Setup
+
+Enable Corepack to use [pnpm](https://pnpm.io/) as a package manager.
+
+```
+corepack enable && corepack enable npm
+```
+
+Install the dependencies:
+
+```bash
+pnpm install
+```
+
+### Develop
+
+Start the development server with:
+
+```bash
+pnpm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+## Codegen
+
+Save your GraphQL queries and mutations in one or more `.graphql` files.
+
+Then, run the codegen script:
+
+```bash
+pnpm run codegen
+```
+
+This will result in the creation of the `src/graphqlTypes.ts` file that contains all the types from your Tailor application.
+
+Additionally, for each `.graphql` file, a similarly named `.generated.graphql` file will be generated at the same location.
+This file contains a set of functions to call your Tailor application's API easily.
+
+The configuration for graphql-codegen is found in [codegen.ts](./codegen.ts).
---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[f_f_f.3rem_#ffffff70]"
+          src="/tailor.png"
+          alt="Tailor Logo"
+          width={332}
+          height={100}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
+        <a
+          href="https://docs.tailor.tech"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about the Tailor Platform.
+          </p>
+        </a>
+
+        <a
+          href="https://www.tailor.tech/templates"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore ERP templates to jump start your Tailor application.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
