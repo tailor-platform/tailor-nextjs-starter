0328174da19521cbcb52447f961fca46284815a3
IzumiSy <beetle-noise@gmx.com>
2024-01-16 13:42:44 +0900

    Ignore *.config.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,7 @@ const devConfig = require("@tailor-platform/dev-config/eslint");
 
 module.exports = {
   ...devConfig,
-  ignorePatterns: [
-    ...devConfig.ignorePatterns,
-    "tailwind.config.js",
-    "next.config.js",
-    "postcss.config.js",
-  ],
+  ignorePatterns: [...devConfig.ignorePatterns, "*.config.js"],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
